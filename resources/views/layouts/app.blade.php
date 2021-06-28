1a0452c442aac36c28fdcdf52de56262b4953d0c
PranavBhatia <bhatiapranav1996@gmail.com>
2021-06-28 03:37:38 -0400

    Authenticated state

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,18 +24,23 @@
             </li>
         </ul>
         <ul class="flex items-center">
-            <li>
-                <a href="" class="p-3">Pranav Bhatia</a>
-            </li>
-            <li>
-                <a href="" class="p-3">Login</a>
-            </li>
-            <li>
-                <a href="{{ route('register') }}" class="p-3">Register</a>
-            </li>
-            <li>
-                <a href="" class="p-3">Logout</a>
-            </li>
+            @auth
+                <li>
+                    <a href="" class="p-3">Pranav Bhatia</a>
+                </li>
+                <li>
+                    <a href="" class="p-3">Logout</a>
+                </li>
+            @endauth
+
+            @guest
+                <li>
+                    <a href="" class="p-3">Login</a>
+                </li>
+                <li>
+                    <a href="{{ route('register') }}" class="p-3">Register</a>
+                </li>
+            @endguest
         </ul>
     </nav>
     @yield('content')
