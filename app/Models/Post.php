f2f5c5c5cf637aeed3408715bb26414838f77042
PranavBhatia <bhatiapranav1996@gmail.com>
2021-06-29 09:55:18 -0400

    Liking and unliking posts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,8 +2,8 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
@@ -11,8 +11,18 @@ class Post extends Model
 
     protected $fillable = ['body'];
 
+    public function likedBy(User $user)
+    {
+        return $this->likes->contains('user_id', $user->id);
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
     }
 }
